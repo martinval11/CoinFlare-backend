ea211c227f30b301f7a3ed51cb0d3a86797217fe
tux <tux@tux.localdomain>
2023-08-13 16:40:35 -0300

    init

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express, { json } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import helmet from 'helmet';
+import compression from 'compression';
+import dotenv from 'dotenv';
+
+import router from './routes';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+const DB_URI = process.env.DB_URI ?? '';
+
+app.use(compression());
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  })
+);
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    app.use(json());
+    app.use('/api', router);
+    app.listen(PORT);
+    console.log(`Server running on port ${PORT}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ routes.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import User from './models/User';
+
+const router = express.Router();
+
+router.get('/users', async (_req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.send('Error getting Users');
+    console.error(error);
+  }
+});
+
+router.post('/createUser', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.send('Error creating User');
+    console.error(error);
+  }
+});
+
+router.patch('/updateUser/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.send(user);
+  } catch (error) {
+    res.send('Error updating User');
+    console.error(error);
+  }
+});
+
+router.delete('/deleteUser/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.send(user);
+  } catch (error) {
+    res.send('Error deleting User');
+    console.error(error);
+  }
+});
+
+export default router;
