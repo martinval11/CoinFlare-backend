ea053f742cefef3cd15fcbbbd4c972db7bd8eedc
tux <tux@tux.localdomain>
2023-08-21 21:30:10 -0300

    build

---
+++ build/models/User.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var mongoose_1 = __importDefault(require("mongoose"));
+var userSchema = new mongoose_1["default"].Schema({
+    name: String,
+    email: String,
+    password: String,
+    portfolio: {
+        type: Array,
+        "default": []
+    }
+});
+exports["default"] = mongoose_1["default"].model('User', userSchema);
